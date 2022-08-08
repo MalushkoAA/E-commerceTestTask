ec85747b9c6cb8b29deef90cdb54c7ca088103c8
Aleksey Malushko <alekseymalushko@gmail.com>
2022-08-08 11:15:55 +0300

    update screenshots

--- app/src/main/java/com/malushkoaa/e_commercetesttask/MainActivity.kt
+++ app/src/main/java/com/malushkoaa/e_commercetesttask/MainActivity.kt
@@ -2,11 +2,13 @@ package com.malushkoaa.e_commercetesttask
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
     }
 
